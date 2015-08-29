f0511a4f3d7ebd9343fcd2323526e48165a1347e
pansay <jan.krepelka@gmail.com>
2015-08-29 21:36:17 +0200

    tdd3

--- specs/app.js
+++ specs/app.js
@@ -1,9 +1,24 @@
-/* global describe, it, angular, expect */
+/* global describe, it, angular, expect, module, beforeEach */
 
 describe('ngSandbox', function() {
 
+    var injected = {};
+
     it('should be defined', function() {
         expect(angular.module('sandbox')).toBeDefined();
+    });
+
+    beforeEach(module('sandbox'));
+    beforeEach(function($injector) {
+        injected.factory = $injector.get('unicornLauncherFactory');
+    });
+
+    describe('unicornLauncherFactory', function() {
+
+        it('should be defined', function() {
+            expect(injected.factory).toBeDefined();
+        });
+
     });
 
 });
